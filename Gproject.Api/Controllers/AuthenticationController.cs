0169b1b5fc1aaa4fa82770172079dfa59f79e3e8
Husseinatta <ahussein@meemdev.com>
2022-10-19 17:34:38 +0200

    Add project files.

---
+++ Gproject.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Gproject.contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gproject.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            return Ok(request);
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            return Ok(request);
+        }
+    }
+}
